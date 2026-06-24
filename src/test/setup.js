b4f2bfd01374530a301e41cb10771e860d42c9d2
Solowayy <denispritula1010@gmail.com>
2026-06-24 23:57:12 +0200

    clean up

--- src/test/setup.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const mockPath = path.resolve(__dirname, 'mocks/vscode.js');
-
-require.cache['vscode'] = {
-    id: 'vscode',
-    filename: mockPath,
-    loaded: true,
-    exports: require(mockPath)
-};
